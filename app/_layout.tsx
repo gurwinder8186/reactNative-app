e6e2cd83197de020635c4e8af421a8c50e0317c1
Gurwinder Singh <gurwinder8186@gmail.com>
2025-05-16 00:40:59 +1200

    remove header from home page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout() {
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
         {/* <Stack.Screen name="(tabs)" options={{ headerShown: false }} /> */}
-        <Stack.Screen name='index' options={{title:'Home'}}/>
+        <Stack.Screen name='index' options={{title:'Home', headerShown: false}}/>
         <Stack.Screen name='contact' options={{title:'Contact Us'}}/>
         <Stack.Screen name="+not-found" />
       </Stack>
