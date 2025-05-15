2974a0eb2b460c1ef0401a769f7a5cf19698b4a7
Gurwinder Singh <gurwinder8186@gmail.com>
2025-05-16 00:17:00 +1200

    add routing

--- app/index.jsx
+++ app/index.jsx
@@ -1,4 +1,5 @@
 import { View, Text, StyleSheet, ImageBackground } from 'react-native'
+import { Link } from 'expo-router'
 import icedCoffeeImg from "@/assets/images/iced-coffee.png"
 
 const app = () => {
@@ -9,7 +10,8 @@ const app = () => {
       resizedMode="cover"
       style={styles.image}
       >
-      <Text style={styles.text}>Coffee Shop</Text>
+      <Text style={styles.title}>Coffee Shop</Text>
+      <Link href= "/explore" style={styles.link}>Explore</Link>
       </ImageBackground>
       <Text>Coffee shop</Text>
     </View>
@@ -30,11 +32,22 @@ resizeMode: 'cover',
 justifyContent: 'center',
 
   },
-  text: {
+  title: {
     color: 'white',
     fontSize: 42,
     fontWeigth: 'bold',
     textAlign: 'center',
     backgroundColor: 'rgba(0,0,0,0.2)',
+    marginBottom: 120,
+  },
+  link: {
+    color: 'white',
+    fontSize: 42,
+    fontWeigth: 'bold',
+    textAlign: 'center',
+    textDecorationLine: 'underline',
+    backgroundColor: 'rgba(0,0,0,0.2)',
+    padding: 4,
   }
+
 })
