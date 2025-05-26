e22f7fafe8812c26f5338bae1ff65635da534ced
Gurwinder Singh <gurwinder8186@gmail.com>
2025-05-26 23:28:19 +1200

    remove tabs navigation with pages

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,15 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import * as SplashScreen from 'expo-splash-screen'
+import { Appearance } from 'react-native';
+import { Colors } from '@/constants/Colors';
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
+  const colorScheme = Appearance.getColorScheme()
+
+  const theme = colorScheme=== 'dark' ? Colors.dark : Colors.light;
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
@@ -18,15 +20,11 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        {/* <Stack.Screen name="(coffee)" options={{ headerShown: false }} /> */}
-        {/* <Stack.Screen name='index' options={{title:'Home', headerShown: false}}/>
-        <Stack.Screen name='contact' options={{title:'Contact Us'}}/>
-        <Stack.Screen name="+not-found" />*/}
+      <Stack screenOptions={{headerStyle: {backgroundColor: theme.headerBackground}, headerTintColor: theme.text, headerShadowVisible: false}}>
+        <Stack.Screen name="index" options={{ headerShown: false, title: 'Home' }} />
+        <Stack.Screen name="menu" options={{ headerShown: true, title: 'menu', headerTitle: 'Cofee Shop Menu' }} />
+        <Stack.Screen name="contact" options={{ headerShown: true, title: 'contact', headerTitle: 'Contact Us' }} />
+        <Stack.Screen name="+not-found" />
       </Stack> 
-      <StatusBar style="auto" />
-    </ThemeProvider>
   );
 }
